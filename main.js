e282e4393ee9389baef2434e43d5b3f799407046
Brice TENCÉ <brice@naturabuy.fr>
2023-02-09 19:32:47 +0100

    better but not parfect (beer buggy when window is not verry wide)

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
-onload = async () => {
-  let percentage = 0;
-  while (true) {
-    percentage += 10;
-    if (percentage > 100) {
-      percentage = 0;
-    }
-
-    fillImg("beer-mayo", percentage);
-
-    await sleep(2000);
-  }
-};
-
-const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
 const fillImg = async (target, percentage) => {
   anime({
     targets: `#${target} .fill`,
@@ -50,30 +34,46 @@ function currentPageIs(pageId) {
 
 
 function incrScore(incrVal, team) {
-        var newScore = currentScore[team] + incrVal;
-        newScore = Math.max(0, newScore);
-        newScore = Math.min(newScore, maxScore);
+    var newScore = currentScore[team] + incrVal;
+    newScore = Math.max(0, newScore);
+    newScore = Math.min(newScore, maxScore);
 
-        currentScore[team] = newScore;
+    currentScore[team] = newScore;
 
-        updateScoreMarkup(team);
+    updateScoreMarkup(team);
+
+    setFillLeft();
 }
 
 function updateScoreMarkup(team) {
-        let scoreNumberNode = document.getElementById('score-number-' + team);
-        scoreNumberNode.innerHTML = currentScore[team].toLocaleString('fr-FR', {
-                minimumIntegerDigits: 2,
-                useGrouping: false
-            });
+    let scoreNumberNode = document.getElementById('score-number-' + team);
+    scoreNumberNode.innerHTML = currentScore[team].toLocaleString('fr-FR', {
+            minimumIntegerDigits: 2,
+            useGrouping: false
+        });
+    fillImg('beer-' + team, currentScore[team] / maxScore * 100);
+}
+
+function setFillLeft() {
+    if(currentPageIs('page-score')) {
+        let fillNode = null;
+        for(const team in currentScore) {
+            fillNode = document.querySelector('#beer-' + team + ' img.fill');
+            fillNode.style.left = 'calc(' + (-fillNode.offsetWidth + 'px') + ' + 25%';
+        }
+    }
 }
 
 
 (function() {
-
     // init currentPageIndex according to hash
     if(window.location.hash) {
        currentPageIndex = Math.max(0, pagesOrder.indexOf(window.location.hash.replace('#', '')));
     }
+
+    window.addEventListener("resize", setFillLeft);
+    window.addEventListener("hashchange", setFillLeft);
+
 
     document.addEventListener("keydown", checkKey);
     function checkKey(e) {
@@ -86,40 +86,42 @@ function updateScoreMarkup(team) {
             // down arrow
         }
         else if (e.keyCode == '37') {
-                // left arrow
-                var nextPageIndex = Math.max(currentPageIndex-1, 0);
-                currentPageIndex = nextPageIndex;
-                window.location.hash = pagesOrder[currentPageIndex];
+            // left arrow
+            var nextPageIndex = Math.max(currentPageIndex-1, 0);
+            currentPageIndex = nextPageIndex;
+            window.location.hash = pagesOrder[currentPageIndex];
+            setFillLeft();
         }
         else if (e.keyCode == '39') {
-                // right arrow
-                var nextPageIndex = Math.min(currentPageIndex+1, pagesOrder.length - 1);
-                currentPageIndex = nextPageIndex;
-                window.location.hash = pagesOrder[currentPageIndex];
+            // right arrow
+            var nextPageIndex = Math.min(currentPageIndex+1, pagesOrder.length - 1);
+            currentPageIndex = nextPageIndex;
+            window.location.hash = pagesOrder[currentPageIndex];
+            setFillLeft();
         }
         else if (e.keyCode == '65') {
-                // letter A
-                if(currentPageIs('page-score')) {
-                        incrScore(-1, 'ketchup');
-                }
+            // letter A
+            if(currentPageIs('page-score')) {
+                incrScore(-1, 'ketchup');
+            }
         }
         else if (e.keyCode == '69') {
-                // letter E
-                if(currentPageIs('page-score')) {
-                        incrScore(1, 'ketchup');
-                }
+            // letter E
+            if(currentPageIs('page-score')) {
+                incrScore(1, 'ketchup');
+            }
         }
         else if (e.keyCode == '87') {
-                // letter W
-                if(currentPageIs('page-score')) {
-                        incrScore(-1, 'mayo');
-                }
+            // letter W
+            if(currentPageIs('page-score')) {
+                incrScore(-1, 'mayo');
+            }
         }
         else if (e.keyCode == '67') {
-                // letter W
-                if(currentPageIs('page-score')) {
-                        incrScore(1, 'mayo');
-                }
+            // letter W
+            if(currentPageIs('page-score')) {
+                incrScore(1, 'mayo');
+            }
         }
     }
 })();
